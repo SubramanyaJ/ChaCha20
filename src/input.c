b581a808df2754970ddae14cd66b22d6ca59ad69
SubramanyaJ <subramanyaj7@gmail.com>
2025-04-08 17:25:34 +0530

    added key input

---
+++ src/input.c
@@ -0,0 +1,53 @@
+#include "input.h"
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include <stdint.h>
+
+uint8_t *
+get_key() {
+	char buffer[256];
+	uint8_t *key = calloc(32, sizeof(uint8_t));
+
+	fprintf(stdout, "Enter password: ");
+
+	// Remove newline if present
+	size_t len = strlen(buffer);
+	if (len > 0 && buffer[len - 1] == '\n') {
+		buffer[len - 1] = '\0';
+		len--;
+	}
+
+	if (len >= 32) {
+		memcpy(key, buffer, 32);
+	} else {
+		memcpy(key, buffer, len);
+	}
+
+	return key;
+}
+
+uint8_t *get_nonce() {
+	uint8_t *nonce = calloc(12, sizeof(uint8_t));
+
+	int fd = open("/dev/urandom", O_RDONLY);
+	if (fd < 0) {
+random_failure:
+		fprintf(stderr, "Warning: /dev/urandom could not be used.\nUsing stdlib's rand() instead.\n");
+		for (int i = 0; i < 12; i++) {
+			nonce[i] = rand();
+		}
+		return nonce;
+	}
+
+	ssize_t bytes_read = read(fd, nonce, 12);
+	close(fd);
+
+	if (bytes_read != 12) {
+		goto random_failure;
+	}
+
+	return nonce;
+}
---
+++ src/input.h
@@ -0,0 +1,13 @@
+#ifndef INPUT_H
+#define INPUT_H
+
+#include <stdint.h>
+#include <stdio.h>
+
+uint8_t *
+get_key();
+
+uint8_t *
+get_nonce();
+
+#endif /* input.h included */
